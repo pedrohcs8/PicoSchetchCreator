d282c8a0e6923296e83d1fcc52aaf3c11d6e0105
Pedro Sena <76856934+pedrohcs8@users.noreply.github.com>
2025-01-19 21:30:27 -0400

    Delete unecessary dir

--- src/teste/compile.sh
+++
@@ -1,8 +0,0 @@
-rm -rf build/*
-cd build
-cmake ..
-bear -- make
-rm -rf ../compile_commands.json
-cp compile_commands.json ..
-cp teste.uf2 /media/pedrohcs8/RPI-RP2
-$shell
--- src/teste/serialmonitor.sh
+++
@@ -1,1 +0,0 @@
-sudo minicom -b 115200 -o -D /dev/ttyACM0
